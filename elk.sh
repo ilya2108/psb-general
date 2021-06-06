bc30dac2cad781a94d99adde68a5e7ba25f25622
Ilya Ryabukhin <ryabuily@fit.cvut.cz>
2021-06-06 09:36:02 +0200

    wrong namespace delete

--- elk.sh
+++ elk.sh
@@ -1,5 +1,5 @@
 helm repo add elastic https://helm.elastic.co
-helm install -n learnshell elasticsearch elastic/elasticsearch --set service.type=LoadBalancer
-helm install -n learnshell kibana elastic/kibana --set service.type=LoadBalancer
-helm install -n learnshell metricbeat elastic/metricbeat 
-helm install -n learnshell  filebeat elastic/filebeat
+helm install elasticsearch elastic/elasticsearch --set service.type=LoadBalancer
+helm install kibana elastic/kibana --set service.type=LoadBalancer
+helm install metricbeat elastic/metricbeat 
+helm install filebeat elastic/filebeat
